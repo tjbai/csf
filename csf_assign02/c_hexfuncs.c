34dacdc8cee0ea928e2e06b12c76d7f9d2ac5995
TJ Bai <tjianbai@gmail.com>
2023-02-12 22:49:50 -0500

    hexread

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -1,6 +1,12 @@
 // C implementation of hexdump functions
 
-#include <unistd.h>  // this is the only system header file you may include!
 #include "hexfuncs.h"
+#include <unistd.h> // this is the only system header file you may include!
 
-// TODO: add function implementations here
+unsigned hex_read(char data_buf[]) { return (unsigned)read(0, data_buf, 16); }
+
+void hex_write_string(const char s[]) {}
+
+void hex_format_offset(unsigned offset, char sbuf[]);
+void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]);
+char hex_to_printable(unsigned char byteval);
