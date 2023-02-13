50f3c7923925bd8b9d88f6646737883a68e73f72
TJ Bai <tjianbai@gmail.com>
2023-02-12 23:22:15 -0500

    preliminary implementations of c_hexfuncs

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -11,12 +11,28 @@ void hex_write_string(const char s[]) {
   int len = 0;
   while (s[len] != '\0')
     ++len;
-  write(STDOUT_FILENO, s, len);
+
+  char sbuf[len];
+  for (int i = 0; i < len; ++i) {
+    sbuf[i] = hex_to_printable(s[i]);
+  }
+
+  write(STDOUT_FILENO, sbuf, len);
 }
 
-void hex_format_offset(unsigned offset, char sbuf[]);
+void hex_format_offset(unsigned offset, char sbuf[]) {
+  sbuf[8] = '\0';
+  for (int i = 7; i >= 0; --i) {
+    sbuf[i] = (offset & 0xF) + '0';
+    offset >>= 4;
+  }
+}
 
-void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]);
+void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]) {
+  sbuf[1] = (byteval & 0xF) + '0';
+  byteval >>= 4;
+  sbuf[0] = (byteval & 0xF) + '0';
+}
 
 char hex_to_printable(unsigned char byteval) {
   if (byteval >= 32 && byteval <= 126) {
