9587dfe13efb056fc2c35413ed1f3d894cff85e2
TJ Bai <tjianbai@gmail.com>
2023-03-08 23:45:15 -0500

    really ugly but works for some bare cases

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -17,8 +17,8 @@
 #include <vector>
 
 struct Slot {
-  int access_ts, load_ts;
-  bool dirty = false;
+  int used_ts, creation_ts;
+  bool dirty;
 };
 
 typedef std::unordered_map<int, Slot> set_t;
@@ -61,32 +61,70 @@ int main(int argc, char *argv[]) {
   int store_count = 0, store_hit = 0;
   int cycle_count = 0;
 
-  unsigned int time = 0;
+  int time = 0;
   std::string trace;
 
   // TODO: increment clock cycles
   while (std::getline(std::cin, trace)) {
     char instruction = trace[0];
-    int address = std::stoul(trace.substr(2, 10), NULL, 16);
+    unsigned int address = std::stoul(trace.substr(2, 10), NULL, 16);
 
-    int index = (address >> offset_bits) & (set_count - 1);
-    int tag = address >> (offset_bits + index_bits);
+    unsigned int index = (address >> offset_bits) & (set_count - 1);
+    unsigned int tag = address >> (offset_bits + index_bits);
 
+    std::cout << instruction << " " << tag << " " << index << std::endl;
+
+    int cache_hit = cache[index].count(tag);
+
+    // LOAD FROM CACHE
     if (instruction == 'l') {
       ++load_count;
-      if (cache[index].count(tag)) {
-        ++load_hit;
+      if (cache_hit) {
+        cache[index][tag].used_ts = time;
+        ++load_hit; 
+        // ++cycle_count; 
+        std::cout << "JUST LOADED FROM CACHE" << std::endl;
       }
       else {
         load_into_cache(cache[index], blocks_per_set, time, tag, lru, cycle_count);
+        // cycle_count += 100;
+        std::cout << "JUST LOADED FROM MEMORY" << std::endl;
       }
-    } else {
-      if (cache[index].count(tag)) continue;
-      else continue;
+    } 
+    // WRITE TO CACHE
+    else {
+      ++store_count;
+      if (cache_hit) {
+        cache[index][tag].used_ts = time;
+        ++store_hit;
+        if (!write_back) {
+          // cycle_count += 101;
+        }
+        else {
+          // ++cycle_count;
+          cache[index][tag].dirty = true;
+        }
+        std::cout << "WROTE TO CACHE" << std::endl;
+      }
+      else {
+        if (write_allocate) {
+          load_into_cache(cache[index], blocks_per_set, time, tag, lru, cycle_count);
+        }
+        else {
+          // cycle_count += 100;
+        }
+      };
     }
-
     ++time;
   }
+
+  std::cout << "Total loads: " << load_count << std::endl; 
+  std::cout << "Total stores: " << store_count << std::endl;
+  std::cout << "Load hits: " << load_hit << std::endl;
+  std::cout << "Load hits: " << (load_count - load_hit) << std::endl;
+  std::cout << "Store hits: " << store_hit << std::endl;
+  std::cout << "Store misses: " << (store_count - store_hit) << std::endl;
+  std::cout << "Total cycles: " << cycle_count << std::endl;
 }
 
 bool pow2(int val, int limit) {
@@ -95,29 +133,38 @@ bool pow2(int val, int limit) {
 }
 
 int erase_from_full_set(set_t &set, bool lru, int &cycle_count) {
-  // TODO: check for "dirty" bit
-
   int rep_tag = -1;
   int last_used_time = std::numeric_limits<int>::max();
 
   for (std::pair<int, Slot> kv : set) {
-    if (lru && kv.second.access_ts < last_used_time) {
+    if (lru && kv.second.used_ts < last_used_time) {
       rep_tag = kv.first;
-      last_used_time = kv.second.access_ts;
+      last_used_time = kv.second.used_ts;
+
+      std::cout << "ITERATION BEFORE ERROR" << kv.first << std::endl;
     }
 
-    if (!lru && kv.second.load_ts < last_used_time) {
+    if (!lru && kv.second.creation_ts < last_used_time) {
       rep_tag = kv.first;
-      last_used_time = kv.second.load_ts;
+      last_used_time = kv.second.creation_ts;
+
+      std::cout << "ITERATION BEFORE ERROR" << kv.first << std::endl;
     }
   }
 
+  if (rep_tag == -1) {
+    std::cout << "SOMETHING REALLY WENT WRONG HERE" << std::endl;
+    exit(1);
+  }
+
+  std::cout << "REPLACING FROM FULL CACHE SET" << std::endl;
+
+  // cycle_count += set[rep_tag].dirty ? 100 : 0;
   set.erase(rep_tag);
   return rep_tag;
-} 
+}
 
-void load_into_cache(set_t &set, int max_size, int time, 
-                    int tag, bool lru, int &cycle_count) {
-  if (set.size() == max_size) erase_from_full_set(set, lru);
-  set[tag] = {time, time};
+void load_into_cache(set_t &set, int max_size, int time, int tag, bool lru, int &cycle_count) {
+  if (set.size() == max_size) erase_from_full_set(set, lru, cycle_count);
+  set[tag] = {time, time, false};
 }
